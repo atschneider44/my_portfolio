3cf9ea8fd988116dec0964af78fd7bd2ed37ccfa
atschneider44 <at.schneider44@gmail.com>
2023-03-24 13:49:20 +0100

    fixing last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import GroupProject from "./pages/GroupProject.jsx";
-import IndividualProject from "./pages/IndividualProject.jsx";
-import InternationalProject from "./pages/InsternationalProject.jsx";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/groupproject" element={<GroupProject />} />
-          <Route path="/individualproject" element={<IndividualProject />} />
-          <Route path="/internationalproject" element={<InternationalProject />} />
-        </Routes>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
